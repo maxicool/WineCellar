116028b2830243854c7a62abd0b5ac716930b77e
Justin Zhu <maxicool6@hotmail.com>
2017-10-10 21:42:21 +0800

    alpha version

--- WineCelllar/My Project/Application.Designer.vb
+++ WineCelllar/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WineCelllar.Form1
+            Me.MainForm = Global.WineCelllar.frm_winecellar
         End Sub
     End Class
 End Namespace
---
+++ WineCelllar/frm_wine.vb
@@ -0,0 +1,3 @@
+﻿Public Class frm_wine
+
+End Class
